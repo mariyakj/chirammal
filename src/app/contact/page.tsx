e403b928c5e8376f75dbbeaf9d20b5ee7ab0bd4b
mariyakj <151432612+mariyakj@users.noreply.github.com>
2025-03-02 15:08:33 +0530

    email

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -33,7 +33,7 @@ export default function ContactPage() {
     
     try {
       // Send data to your email endpoint
-      const response = await fetch('https://chirammal.vercel.app/send-email', {
+      const response = await fetch('https://chirammal.vercel.app/api/send-email', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
