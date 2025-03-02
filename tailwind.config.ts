6726a80819a29659c683de0df6b272002032ee32
mariyakj <151432612+mariyakj@users.noreply.github.com>
2025-03-02 11:14:50 +0530

    thi

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,7 @@ export default {
   content: [
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
